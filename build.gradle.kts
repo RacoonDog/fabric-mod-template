7a7ce4b79b6d569a1538ee1ff2549ab60f54cef2
RacoonDog <32882447+RacoonDog@users.noreply.github.com>
2024-02-21 21:38:03 -0500

    configure maven-publish

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
 	id("fabric-loom") version "1.5-SNAPSHOT"
+	id("maven-publish")
 }
 
 base {
@@ -52,7 +53,27 @@ tasks {
 	}
 
 	java {
+		withSourcesJar()
+		withJavadocJar()
+
 		sourceCompatibility = JavaVersion.VERSION_17
 		targetCompatibility = JavaVersion.VERSION_17
+	}
+}
+
+// configure the maven publication
+publishing {
+	publications {
+		register("mavenJava", MavenPublication::class) {
+			from(components["java"])
+		}
+	}
+
+	// See https://docs.gradle.org/current/userguide/publishing_maven.html for information on how to set up publishing.
+	repositories {
+		// Add repositories to publish to here.
+		// Notice: This block does NOT have the same function as the block in the top level.
+		// The repositories here will be used for publishing your artifact, not for
+		// retrieving dependencies.
 	}
 }
