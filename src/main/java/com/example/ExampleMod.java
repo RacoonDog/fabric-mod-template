9e9a890189f67db333366d24886e99dcc4138eff
RacoonDog <32882447+RacoonDog@users.noreply.github.com>
2024-02-21 21:22:54 -0500

    fmj

--- src/main/java/com/example/ExampleMod.java
+++ src/main/java/com/example/ExampleMod.java
@@ -1,5 +1,6 @@
 package com.example;
 
+import com.mojang.logging.LogUtils;
 import net.fabricmc.api.ModInitializer;
 
 import org.slf4j.Logger;
@@ -9,7 +10,7 @@ public class ExampleMod implements ModInitializer {
 	// This logger is used to write text to the console and the log file.
 	// It is considered best practice to use your mod id as the logger's name.
 	// That way, it's clear which mod wrote info, warnings, and errors.
-    public static final Logger LOGGER = LoggerFactory.getLogger("modid");
+    public static final Logger LOGGER = LogUtils.getLogger();
 
 	@Override
 	public void onInitialize() {
